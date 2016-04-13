2901ec415b97576d8f4b94f78e3771d79c087654
Andrea Parodi <andrea@parro.it>
2016-04-13 21:09:34 +0200

    first release

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 'use strict';
 
-module.exports = function electronBuiltins() {
-	return 42;
-};
+module.exports = [
+	// API root
+	'electron',
+
+	// common modules
+	'clipboard',
+	'crash-reporter',
+	'deprecations',
+	'nativeImage',
+	'shell',
+
+	// browser-side modules
+	'app',
+	'auto-updater',
+	'browser-window',
+	'content-tracing',
+	'dialog',
+	'ipc-main',
+	'global-shortcut',
+	'menu',
+	'menu-item',
+	'power-monitor',
+	'power-save-blocker',
+	'protocol',
+	'screen',
+	'session',
+	'tray',
+	'navigation-controller',
+	'web-contents',
+
+	// Renderer side modules
+	'desktop-capturer',
+	'ipc-renderer',
+	'remote',
+	'screen',
+	'web-frame'
+];
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const test = require('ava');
-const electronBuiltins = require('./');
+import test from 'ava';
+import builtins from '.';
 
-test('it work!', t => {
-	const result = electronBuiltins();
-	t.is(result, 42);
-});
+test('return an array of modules', t => t.pass(Array.isArray(builtins)));
+test('define new electron facade', t => t.pass(builtins.indexOf('electron') !== -1));
+test('define deprecated exported modules', t => t.pass(builtins.indexOf('app') !== -1));
+test('bring your own node builtins', t => t.pass(builtins.indexOf('fs') === -1));
